6985490edfd1d81bc0001cb3843c37056fadd2f3
srinathsuddala <srinathsuddala@gmail.com>
2020-07-17 16:15:58 +0530

    created ImageFragment.java file

---
+++ app/src/main/java/com/example/note_saviours_android/ImageFragment.java
@@ -0,0 +1,50 @@
+package com.example.note_saviours_android;
+
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+public class ImageFragment extends DialogFragment {
+
+    public static final String EXTRA_IMAGE_PATH =
+        "com.saviours.android.notes.path";
+
+    public static ImageFragment newInstance(String imagePath) {
+        Bundle args = new Bundle();
+        args.putSerializable(EXTRA_IMAGE_PATH, imagePath);
+
+        ImageFragment fragment = new ImageFragment();
+        fragment.setArguments(args);
+        fragment.setStyle(DialogFragment.STYLE_NO_TITLE, 0);
+
+        return fragment;
+    }
+
+    private ImageView mImageView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup parent,
+                             Bundle savedInstanceState) {
+        mImageView = new ImageView(getActivity());
+        String path = (String)getArguments().getSerializable(EXTRA_IMAGE_PATH);
+        BitmapDrawable image = PictureUtils.getScaledDrawable((AppCompatActivity) getActivity(), path);
+
+        mImageView.setImageDrawable(image);
+
+        return mImageView;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        PictureUtils.cleanImageView(mImageView);
+    }
+}
+

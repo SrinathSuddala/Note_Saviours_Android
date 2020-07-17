19de89a4b35694b0ad4d283b7a5c8153d5dfc593
VivekMadishetty07 <madishetty.vivek@gmail.com>
2020-07-17 16:19:47 +0530

    created NoteCameraActivity.java

---
+++ app/src/main/java/com/example/note_saviours_android/NoteCameraActivity.java
@@ -0,0 +1,27 @@
+package com.example.note_saviours_android;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.fragment.app.Fragment;
+
+public class NoteCameraActivity extends SingleFragmentActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        // The calls to requestWindowFeature and addFlags must be made before
+        // the activity's view is created in Activity.setContentView
+        // Hide the window title
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // Hide the status bar and other OS-level chrome
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected Fragment createFragment() {
+        return new NoteCameraFragment();
+    }
+}
